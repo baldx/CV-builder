3031774a430712b4041e6b75cd5a7b01511b5c88
baldx <mr.ahmad.m2006@gmail.com>
2024-01-08 20:16:21 +0100

    import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Forms from './components/form'
 
 function App() {
 
   return (
     <>
-      
+      <Forms></Forms>
     </>
   )
 }
