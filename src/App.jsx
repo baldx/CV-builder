4e7aab99a9fad5e64acac76a5c9f7c8c516cc8e1
baldx <mr.ahmad.m2006@gmail.com>
2024-01-17 19:04:48 +0100

    import top buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Forms from './components/form'
+import CreateButtons from './components/topOption'
 
 function App() {
 
   return (
     <>
+      <CreateButtons></CreateButtons>
       <Forms></Forms>
     </>
   )
