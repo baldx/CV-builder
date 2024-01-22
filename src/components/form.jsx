ae55b49aa63c9b02d44a9d4d710c5e95454c9098
baldx <mr.ahmad.m2006@gmail.com>
2024-01-22 21:09:47 +0100

    fix dynamically update onChange

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -16,93 +16,99 @@ function PersonalInfo ({name, email, phone, address}) {
     )
 }
 
-function CreateForm({type, name, result, startDate, endDate, location, description, number, email}) {
-    
-    const [fullName, setFullName] = useState('asdadökjasdö');
-    const [fullEmail, setFullEmail] = useState('');
-    const [phone, setPhone] = useState('');
-    const [address, setAddress] = useState('');
-    
 
-
-    function handleInputChange (e) {
-        setFullName(e.target.value);
-    }
+function CreateForm({
+    type, name, result, startDate, endDate, location, description, number, email,
+    setFullName, fullName, setFullEmail, fullEmail, setFullNumber, fullNumber,
+}) {
 
     if (startDate || endDate) {
         return (
-            <>
             <form className={type}>
-            <div className="form-title">{type}</div>
-                <label htmlFor={name}>
-                    {name}
-                    <input type="text" name={name} id={name} placeholder={"Enter " + name} value={fullName} onChange={handleInputChange}></input>
-                </label>
-                <label htmlFor={result}>
-                    {result}
-                    <input type="text" name={result} id={result} placeholder={"Enter " + result}/>
-                </label>
-                <label htmlFor={startDate}>
-                    Start date
-                    <input type="text" name={startDate} id={startDate} placeholder={"Enter " + startDate + " date"}/>
-                </label>
-                <label htmlFor={endDate}>
-                    End date
-                    <input type="text" name={endDate} id={endDate} placeholder={"Enter " + endDate + " date"}/>
-                </label>
-                <label htmlFor={location}>
-                    Location
-                    <input type="text" name={location} id={location} placeholder={"Enter " + location}/>
-                </label>
-                <button type="submit" className="submit">
-                    Submit
-                </button>
+                <div className="form-title">{type}</div>
+                    <label htmlFor={name}>
+                        {name}
+                        <input type="text" name={name} id={name} placeholder={"Enter " + name} ></input>
+                    </label>
+                    <label htmlFor={result}>
+                        {result}
+                        <input type="text" name={result} id={result} placeholder={"Enter " + result}/>
+                    </label>
+                    <label htmlFor={startDate}>
+                        Start date
+                        <input type="text" name={startDate} id={startDate} placeholder={"Enter " + startDate + " date"}/>
+                    </label>
+                    <label htmlFor={endDate}>
+                        End date
+                        <input type="text" name={endDate} id={endDate} placeholder={"Enter " + endDate + " date"}/>
+                    </label>
+                    <label htmlFor={location}>
+                        Location
+                        <input type="text" name={location} id={location} placeholder={"Enter " + location}/>
+                    </label>
+                    <button type="submit" className="submit">
+                        Submit
+                    </button>
             </form>
-            </>
         )
     } else {
         return (
             <>
-            <form className={type}>
-            <div className="form-title">{type}</div>
-                <label htmlFor={name}>
-                    {name}
-                    <input type="text" name={name} id={name} placeholder={"Enter " + name} value={fullName} onChange={event => setFullName(event.target.value)}></input>
-                </label>
-                <label htmlFor={email}>
-                    {email}
-                    <input type="text" name={email} id={email} placeholder={"Enter " + email}/>
-                </label>
-                <label htmlFor={number}>
-                    Number
-                    <input type="text" name={number} id={number} placeholder={"Enter " + number}/>
-                </label>
-                <label htmlFor={location}>
-                    Location
-                    <input type="text" name={location} id={location} placeholder={"Enter " + location}/>
-                </label>
-                <button type="submit" className="submit">
-                    Submit
-                </button>
-            </form>
+                <form className={type}>
+                <div className="form-title">{type}</div>
+                    <label htmlFor={name}>
+                        {name}
+                        <input type="text" name={name} id={name} placeholder={"Enter " + name} value={fullName} onChange={e => setFullName(e.target.value)} />
+                    </label>
+                    <label htmlFor={email}>
+                        {email}
+                        <input type="text" name={email} id={email} placeholder={"Enter " + email} value={fullEmail} onChange={e => setFullEmail(e.target.value)}/>
+                    </label>
+                    <label htmlFor={number}>
+                        Number
+                        <input type="text" name={number} id={number} placeholder={"Enter " + number} value={fullNumber} onChange={e => setFullNumber(e.target.value)}/>
+                    </label>
+                    <label htmlFor={location}>
+                        Location
+                        <input type="text" name={location} id={location} placeholder={"Enter " + location}/>
+                    </label>
+                    <button type="submit" className="submit">
+                        Submit
+                    </button>
+                </form>
             </>
         )
+
     }
+
 
 }
 
 export default function Forms() {
+
+    const [fullName, setFullName] = useState('Johnny Cena');
+    const [fullEmail, setFullEmail] = useState('Ibbeisgay@gmail.com');
+    const [fullNumber, setFullNumber] = useState('6969696969');
+    const [address, setAddress] = useState('');
+
     return (
         <>
             <div className="forms">
                 <CreateButtons />
-                <CreateForm type="Personal" name="Name" email="Email" number="Number" location="location"/>
+                <CreateForm type="Personal" name="Name" email="Email" number="Number" location="location" setFullName={setFullName} fullName={fullName} setFullEmail={setFullEmail} fullEmail={fullEmail} setFullNumber={setFullNumber} fullNumber={fullNumber}/>
                 <CreateForm type="Education" name="School" result="Degree" startDate="Start" endDate="End" location="location" />
-                <CreateForm type="Experience" name="Company" result="Position" startDate="Start" endDate="End" location="location" />
+                <CreateForm type="Experience" name="Company" result="Position" startDate="Start" endDate="End" location="location" description="description" />
             </div>
 
             <div className="CV">
-                <PersonalInfo name={"dsad"}/>
+            <header>
+                <div className="name">{fullName}</div>
+                <div className="personal-other">
+                    <div className="email">{fullEmail}</div>
+                    <div className="phone">{fullNumber}</div>
+                    <div className="address"></div>
+                </div>
+            </header>
             </div>
         </>
     )
