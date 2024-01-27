c049a1eddb9d43b214bfd59b9dde4ac4ae78e2b9
baldx <mr.ahmad.m2006@gmail.com>
2024-01-27 19:04:32 +0100

    add click event

--- src/components/topOption.jsx
+++ src/components/topOption.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 export default function CreateOption({type, innerText, clickEvent}) {
     return (
         <>
-            <button className={type + ' ' + 'option'}>{innerText} </button>
+            <button className={type + ' ' + 'option' } onClick={clickEvent}>{innerText} </button>
         </>
     )
 }
