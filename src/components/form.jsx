3517db23f7cc2b3da2ba6d0c292d93350f872f46
baldx <mr.ahmad.m2006@gmail.com>
2024-01-27 19:22:46 +0100

    add clearBtn logic & input limits & change example

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import CreateOption from './topOption';
 
+const inputLength = 30;
+const dateLength = 10;
+const descriptionLength = 200;
+
 function CreateForm({
     type, name, result, startDate, endDate, location, description, number, email,
     setFullName, fullName, setFullEmail, fullEmail, setFullNumber, fullNumber, setLocationPersonal,
@@ -35,27 +39,27 @@ function CreateForm({
                 <div className="form-title">{type}</div>
                     <label htmlFor={name}>
                         {name}
-                        <input type="text" name={name} id={name} placeholder={"Enter " + name} value={school} onChange={e => setSchool(e.target.value)}></input>
+                        <input type="text" name={name} id={name} placeholder={"Enter " + name} value={school} onChange={e => setSchool(e.target.value)} maxLength={inputLength}></input>
                     </label>
                     <label htmlFor={result}>
                         {result}
-                        <input type="text" name={result} id={result} placeholder={"Enter " + result} value={degree} onChange={e => setDegree(e.target.value)}/>
+                        <input type="text" name={result} id={result} placeholder={"Enter " + result} value={degree} onChange={e => setDegree(e.target.value)}  maxLength={inputLength} />
                     </label>
                     <label htmlFor={startDate}>
                         Start date
-                        <input type="text" name={startDate} id={startDate} placeholder={"Enter " + startDate + " date"} value={startSchool} onChange={e => setStartSchool(e.target.value)}/>
+                        <input type="text" name={startDate} id={startDate} placeholder={"Enter " + startDate + " date"} value={startSchool} onChange={e => setStartSchool(e.target.value)} maxLength={dateLength} />
                     </label>
                     <label htmlFor={endDate}>
                         End date
-                        <input type="text" name={endDate} id={endDate} placeholder={"Enter " + endDate + " date"} value={endSchool} onChange={e => setEndSchool(e.target.value)}/>
+                        <input type="text" name={endDate} id={endDate} placeholder={"Enter " + endDate + " date"} value={endSchool} onChange={e => setEndSchool(e.target.value)} maxLength={dateLength}/>
                     </label>
                     <label htmlFor={location}>
                         Location
-                        <input type="text" name={location} id={location} placeholder={"Enter " + location} value={locationSchool} onChange={e => setLocationSchool(e.target.value)}/>
+                        <input type="text" name={location} id={location} placeholder={"Enter " + location} value={locationSchool} onChange={e => setLocationSchool(e.target.value)} maxLength={inputLength}/>
                     </label>
                     <label htmlFor={description}>
                         Description 
-                        <textarea name={description} id={description} cols="30" rows="10" value={description1} onChange={e => setDescription1(e.target.value)}></textarea>
+                        <textarea name={description} id={description} cols="30" rows="10" value={description1} onChange={e => setDescription1(e.target.value)} maxLength={descriptionLength}></textarea>
                     </label>
                 </div>
                     <button type="submit" className="submit" onClick={handleClickSubmit}>
@@ -71,24 +75,23 @@ function CreateForm({
                     <div className="form-title">{type}</div>
                         <label htmlFor={name}>
                             {name}
-                            <input type="text" name={name} id={name} placeholder={"Enter " + name} value={fullName} onChange={e => setFullName(e.target.value)} />
+                            <input type="text" name={name} id={name} placeholder={"Enter " + name} value={fullName} onChange={e => setFullName(e.target.value)} maxLength={inputLength}/>
                         </label>
                         <label htmlFor={email}>
                             {email}
-                            <input type="text" name={email} id={email} placeholder={"Enter " + email} value={fullEmail} onChange={e => setFullEmail(e.target.value)}/>
+                            <input type="text" name={email} id={email} placeholder={"Enter " + email} value={fullEmail} onChange={e => setFullEmail(e.target.value)} maxLength={inputLength}/>
                         </label>
                         <label htmlFor={number}>
                             Number
-                            <input type="text" name={number} id={number} placeholder={"Enter " + number} value={fullNumber} onChange={e => setFullNumber(e.target.value)}/>
+                            <input type="text" name={number} id={number} placeholder={"Enter " + number} value={fullNumber} onChange={e => setFullNumber(e.target.value)} maxLength={inputLength}/>
                         </label>
                         <label htmlFor={location}>
                             Location
-                            <input type="text" name={location} id={location} placeholder={"Enter " + location} value={locationPersonal} onChange={e => setLocationPersonal(e.target.value)}/>
+                            <input type="text" name={location} id={location} placeholder={"Enter " + location} value={locationPersonal} onChange={e => setLocationPersonal(e.target.value)} maxLength={inputLength}/>
                         </label>
                     </div>
                     <button type="submit" className="submit" onClick={handleClickSubmit}>
                         {isSubmittedBtn}
-
                     </button>
                 </form>
             </>
@@ -102,22 +105,22 @@ export default function Forms() {
         fullName: 'Johnny Cena',
         fullEmail: 'JohnnyCena@gmail.com',
         fullNumber: '+4662651951',
-        locationPersonal: 'Zimbabwe',
+        locationPersonal: 'Växjö',
 
         school: 'Thoren framtid',
-        degree: 'Some degree',
-        startSchool: '08/07/23',
-        endSchool: 'present',
-        locationSchool: 'Zambia',
+        degree: 'Gymnasieexamen',
+        startSchool: '08/07/22',
+        endSchool: '13/06/25',
+        locationSchool: 'Växjö',
 
         company: 'ICA',
-        position: 'Waiter',
+        position: 'Cahsier',
         startCompany: '28/01/21',
         endCompany: '03/04/21',
-        locationCompany: 'Angola',
+        locationCompany: 'Växjö',
 
         descriptionSchool: '',
-        descriptionCompany: '',
+        descriptionCompany: 'I had hours during peak times and i my work consisted in taking cashing out customers products. Worked during school break',
     }
 
     const [fullName, setFullName] = useState(initialState.fullName);
@@ -159,11 +162,30 @@ export default function Forms() {
         setDescriptionCompany(initialState.descriptionCompany);
     }
 
+    const handleClear = () => {
+        setFullName('');
+        setFullEmail('');
+        setFullNumber('');
+        setLocationPersonal('');
+        setSchool('');
+        setDegree('');
+        setStartSchool('');
+        setEndSchool('');
+        setLocationSchool('');
+        setCompany('');
+        setPosition('');
+        setStartCompany('');
+        setEndCompany('');
+        setLocationCompany('');
+        setDescriptionSchool('');
+        setDescriptionCompany('');
+    }
+
     return (
         <>
             <div className="forms">
                 <div className="top">
-                    <CreateOption type="clear" innerText="Clear"/>
+                    <CreateOption type="clear" innerText="Clear" clickEvent={handleClear}/>
                     <CreateOption type="example" innerText="Load Example" clickEvent={handleReset}/>
                 </div>
                 <CreateForm type="Personal" name="Name" email="Email" number="Number" location="location" setFullName={setFullName} fullName={fullName} setFullEmail={setFullEmail} fullEmail={fullEmail} setFullNumber={setFullNumber} fullNumber={fullNumber} locationPersonal={locationPersonal} setLocationPersonal={setLocationPersonal}/>
@@ -186,8 +208,7 @@ export default function Forms() {
             <div className="education-bottom">
                 <div className="left">
                     <div className="date">
-                        <div className="start">{startSchool}</div>
-                        <span> - </span>
+                        <div className="start">{startSchool} &nbsp;</div>
                         <div className="end">{endSchool}</div>
                     </div>
                     <div className="location">{locationSchool}</div>
@@ -203,8 +224,7 @@ export default function Forms() {
             <div className="experience-bottom">
                 <div className="left">
                     <div className="date">
-                        <div className="start">{startCompany}</div>
-                        <span> - </span>
+                        <div className="start">{startCompany} &nbsp;</div>
                         <div className="end">{endCompany}</div>
                     </div>
                     <div className="location">{locationCompany}</div>
