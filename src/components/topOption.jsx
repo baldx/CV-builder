f6cc5ec9f67c73221e5ed9262cd833d64db93d26
baldx <mr.ahmad.m2006@gmail.com>
2024-01-27 18:58:30 +0100

    change export function

--- src/components/topOption.jsx
+++ src/components/topOption.jsx
@@ -1,21 +1,9 @@
 import { useState } from 'react'
 
-function CreateOption({type, innerText, clickEvent}) {
+export default function CreateOption({type, innerText, clickEvent}) {
     return (
         <>
-            <button className={type + ' ' + 'option'} onClick={clickEvent}>{innerText} </button>
-        </>
-    )
-}
-
-export default function CreateButtons () {
-    return (
-        <>
-        <section className="top">
-            <CreateOption type="clear" innerText="Clear" />
-            <CreateOption type="example" innerText="Load Example" />
-        </section>
-            
+            <button className={type + ' ' + 'option'}>{innerText} </button>
         </>
     )
 }
